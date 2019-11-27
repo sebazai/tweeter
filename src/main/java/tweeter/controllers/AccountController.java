7a2d42ff9bccbef855add82d89d59d2e2c8866e1
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2019-11-27 13:07:14 +0200

    rename controller

--- src/main/java/tweeter/controllers/AccountController.java
+++ src/main/java/tweeter/controllers/AccountController.java
@@ -5,48 +5,10 @@
  */
 package tweeter.controllers;
 
-import java.util.HashSet;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import tweeter.domain.Account;
-import tweeter.repositories.AccountRepository;
-
 /**
  *
  * @author sebserge
  */
-@Controller
 public class AccountController {
-    @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
     
-    @GetMapping("/register")
-    public String showRegistrationForm() {
-        return "register";
-    }
-    
-    @PostMapping("/register")
-    public String registerUser(Model model, @RequestParam String username, 
-            @RequestParam String password, 
-            @RequestParam String passwordConfirm, 
-            @RequestParam String nickname) {
-        Account a = new Account();
-        System.out.println(username);
-        a.setUsername(username);
-        a.setPassword(passwordEncoder.encode(password));
-        a.setNickname(nickname);
-        accountRepository.save(a);
-        return "redirect:/login";
-    }
 }
